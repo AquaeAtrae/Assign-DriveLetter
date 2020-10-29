482ac42174bff9c78627bcb20ff90a8a413a5cd4
AquaeAtrae <12989575+AquaeAtrae@users.noreply.github.com>
2020-10-29 04:23:47 -0600

    Create Assign-DriveLetter.psm1

---
+++ Assign-DriveLetter.psm1
@@ -0,0 +1,76 @@
+<#
+.Synopsis
+    PowerShell cmdlet to assign a drive letter given the partition's ID, serial, or label
+.Description 
+    This powershell cmdlet 
+	
+.Parameter $DesiredDriveLetter
+    The drive letter to assign to the matching partition
+.Parameter $DriveID
+.Parameter $DriveSerial
+.Parameter $DriveLabel
+
+.Link
+    https://github.com/AquaeAtrae/Assign-DriveLetter
+.Example
+  Import-Module Assign-DriveLetter
+	Assign-DriveLetter -DesiredDriveLetter "D" -DriveLabel "Data"
+	
+.Example
+	Assign-DriveLetter -DesiredDriveLetter "F" -DriveSerial "916668406"
+	
+.Example
+	Assign-DriveLetter -DesiredDriveLetter "X" -DriveID "\\?\Volume{4442da02-898c-11e9-9f9b-981f32013a0e}\"
+	
+#>
+
+function Assign-DriveLetter {
+    param(
+       [Parameter(Position=0,Mandatory)]
+       [string]$DesiredDriveLetter,
+
+       [Parameter(Position=1)]
+       [string]$DriveID,
+
+       [Parameter(Position=2)]
+       [string]$DriveSerial,
+
+       [Parameter(Position=3)]
+       [string]$DriveLabel
+       )
+
+    if (!$DriveID) {  if (!$DriveSerial) {  if (!$DriveLabel) {
+        $Drive = gwmi Win32_Volume | where {$_.Label -like $DriveLabel} | select -First 1
+    } else {
+        $Drive = gwmi Win32_Volume | where {$_.SerialNumber -like $DriveSerial} | select -First 1
+    } else {
+        $Drive = gwmi Win32_Volume | where {$_.DeviceID -like $DriveID} | select -First 1
+    } else {
+        'ERROR: Must identify the drive to assign letter via DriveID, DriveSerial, or DriveLabel'
+    }
+
+    if ($DesiredDriveLetter + ":" -ne $Drive.DriveLetter) {
+        $takenDriveLetters = (Get-PSDrive).Name -like '?'
+        if ($DesiredDriveLetter -in $takenDriveLetters) {
+            # Reassign existing drive to first unused drive letter to release the drive letter
+            $firstUnusedDriveLetter = [char[]] (0x44..0x5a) | where { $_ -notin $takenDriveLetters } | select -first 1  # ASCII letters D through Z
+            
+            Write-Host "Moving existing drive from $DesiredDriveLetter`: to $firstUnusedDriveLetter`:"
+            $ExistingDrive = gwmi Win32_Volume | where {$_.DriveLetter -like $DesiredDriveLetter + ":"}
+
+            if (!$ExistingDrive) {'Boxstarter reboot required'; Invoke-Reboot}  # boxstarter reboot
+            $ExistingDrive.AddMountPoint($firstUnusedDriveLetter + ":\")
+            $ExistingDrive.DriveLetter = $firstUnusedDriveLetter + ":"
+            $ExistingDrive.Put()
+            $ExistingDrive.Mount()
+        }
+
+        if ((Get-PSDrive).Name -like $DesiredDriveLette) {'Boxstarter reboot required'; Invoke-Reboot}  # boxstarter reboot
+
+        Write-Host "Assigning drive to $DesiredDriveLetter`:"
+        $Drive.AddMountPoint($DesiredDriveLetter + ":\")
+        $Drive.DriveLetter = $DesiredDriveLetter + ":"
+        $Drive.Put()
+        $Drive.Mount()
+    }
+}
